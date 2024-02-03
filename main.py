f88b7206d61ef4b2014dcda055ec7795abc99296
balilarder <dokeshino0@gmail.com>
2024-02-03 22:38:14 +0800

    add: query all cubes and their dimensions

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from TM1py.Services import TM1Service
-import typing
+from typing import List
 
 # Read settings from config
 address = 'restapi195.skill.cubewise.asia'
@@ -9,27 +9,28 @@ password = ''
 
 
 # These are query functions
-def list_dimensions(tm1: TM1Service) -> list:
+def list_dimensions(tm1: TM1Service) -> List[str]:
     dimensions = tm1.dimensions.get_all_names(skip_control_dims=True)
-    for dn in dimensions:
-        print(dn)
-
+    
     result = [dn for dn in dimensions]
     return result
 
-def list_cubes(cube_name: list) -> dict:
-    if not cube_name:
-        print("我全都要")
-    else:
-        print("onr by one")
-
-
+def list_cubes_with_dimensions(tm1: TM1Service) -> List[str]:
+    
+    result = {}
+    cubes = tm1.cubes.get_model_cubes()
+    for cube in cubes:
+        result[cube.name] = cube.dimensions
+    return result
+    
 with TM1Service(address=address, port=port, user=user, password=password, ssl=True) as tm1:
-    # print(tm1.server.get_product_Version())
     print(tm1.server.get_product_version())
 
 
     # question1
     all_dimensions = list_dimensions(tm1)
     print(all_dimensions)
+
+    query1 = list_cubes_with_dimensions(tm1)
+    print(query1)
 
