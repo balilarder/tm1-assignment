e40ba52b18442331b8f25c78cba64b263ef80f7c
balilarder <dokeshino0@gmail.com>
2024-02-05 12:18:26 +0800

    add: fastapi for filter the element name

--- demo.py
+++ demo.py
@@ -2,11 +2,19 @@ from fastapi import FastAPI
 app = FastAPI()
 
 from TM1py.Services import TM1Service
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 import pandas as pd
 
 from main import address, port, user, password
 from main import list_dimensions, list_cubes_with_dimensions
+from main import filter_dimension_element
+
+from pydantic import BaseModel
+
+class FilterRequest(BaseModel):
+    dimension_name: str
+    layer_filter: Optional[List[int]] = None
+    conditions: Optional[List[Tuple[str, str, str]]] = None
 
 @app.get('/book/{book_id}')
 def get_book_by_id(book_id: int):
@@ -25,3 +33,15 @@ def get_cube_name_with_dimensions():
     with TM1Service(address=address, port=port, user=user, password=password, ssl=True) as tm1:
         result = list_cubes_with_dimensions(tm1)
     return result
+
+@app.post('/filter_element_name')
+async def get_filtered_element_name(request: FilterRequest):
+    with TM1Service(address=address, port=port, user=user, password=password, ssl=True) as tm1:
+        result = filter_dimension_element(
+            tm1,
+            dimension_name=request.dimension_name,
+            layer_filter=request.layer_filter,
+            conditions=request.conditions
+
+        )
+    return {'length': len(result), 'name': result}
